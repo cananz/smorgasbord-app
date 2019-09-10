1489bc2a78171a41ac9c0fc37c00cd1df5b8b0ba
Pete Hanner <petehanner@gmail.com>
2019-09-09 21:29:32 -0400

    recipe index, full name, starting view links before writing methods

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,4 +1,12 @@
 class RecipesController < ApplicationController
+  def index
+    # TODO: pass @user params from user show page & filter recipes (model method?)
+      # @user = User.find(params[:user])
+      # @recipes = Recipe.user_filtered(@user)
+    @recipes = Recipe.all
+    @user = User.find(params[:user])
+  end
+
   def show
     @recipe = Recipe.find(params[:id])
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,7 @@ class User < ApplicationRecord
   has_many :ingredients, through: :user_ingredients
   accepts_nested_attributes_for :ingredients
 
-
+  def name
+    "#{self.first_name} #{self.last_name}"
+  end
 end
