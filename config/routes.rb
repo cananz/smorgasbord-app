f103f2b3b7a69fdbbdf2145399341c5af0f16165
Pete Hanner <petehanner@gmail.com>
2019-09-10 18:04:57 -0400

    route modding

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :recipes
   resources :users
-  resources :sessions
+  resources :sessions, except: [:index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root 'users#show'
