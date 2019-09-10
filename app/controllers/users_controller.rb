320709b35570119e3440a1d273c091098889a43e
Pete Hanner <petehanner@gmail.com>
2019-09-09 20:58:28 -0400

    user can edit kitchen

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,17 +2,28 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-
   end
 
   def edit
     @user = User.find(params[:id])
-
   end
 
   def update
-
+    @user = User.find(params[:id])
+    kitchen = selected(user_params[:ingredient_ids])
+    @user.ingredients = Ingredient.find(kitchen)
+    redirect_to user_path(@user)
   end
 
+  private
+
+  def selected(arr)
+    arr = arr[1..arr.length]
+    arr.map { |e| e.to_i }
+  end
+
+  def user_params
+    params.require(:user).permit(:id, ingredient_ids:[])
+  end
 
 end
