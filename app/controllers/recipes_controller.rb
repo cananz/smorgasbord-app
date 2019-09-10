d4518c3491279f0f3378242cb5abca5bd19e85c0
Pete Hanner <petehanner@gmail.com>
2019-09-09 21:52:03 -0400

    personalized user recipe show page

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,10 +1,10 @@
 class RecipesController < ApplicationController
   def index
     # TODO: pass @user params from user show page & filter recipes (model method?)
-      # @user = User.find(params[:user])
-      # @recipes = Recipe.user_filtered(@user)
-    @recipes = Recipe.all
     @user = User.find(params[:user])
+    @recipes = Recipe.user_filtered(@user)
+    # @recipes = Recipe.all
+    # @user = User.find(params[:user])
   end
 
   def show
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,21 @@
 class Recipe < ApplicationRecord
   has_many :recipe_ingredients
   has_many :ingredients, through: :recipe_ingredients
+
+  def self.user_filtered(user)
+    # array of hashes to return
+    final = []
+    # loop over each recipe
+    self.all.each do |recipe|
+      recipe_hash = {}
+      recipe_hash[:recipe] = recipe
+      # count number of recipe ingredients missing from user's kitchen
+      diff = recipe.ingredients - user.ingredients
+      recipe_hash[:missing] = diff.length
+      final << recipe_hash
+    end
+    # return sorted array
+    final = final.sort_by { |recipe| recipe[:missing] }
+    final
+  end
 end
