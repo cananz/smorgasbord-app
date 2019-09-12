da493b4d61ec1186f1f2894f8c1f41a0338e0906
Pete Hanner <petehanner@gmail.com>
2019-09-12 17:32:13 -0400

    restricted and unrestricted views for recipes

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,6 +1,11 @@
 class RecipesController < ApplicationController
   def index
-    @recipes = Recipe.user_filtered(current_user)
+    # byebug
+    if params[:restrictions] == "false"
+      @recipes = Recipe.user_include_restrictions(current_user)
+    else
+      @recipes = Recipe.user_filtered(current_user)
+    end
   end
 
   def show
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -7,12 +7,31 @@ class Recipe < ApplicationRecord
     final = []
     # loop over each recipe
     self.all.each do |recipe|
-      recipe_hash = {}
-      recipe_hash[:recipe] = recipe
-      # count number of recipe ingredients missing from user's kitchen
-      diff = recipe.ingredients - user.ingredients
-      recipe_hash[:missing] = diff.length
-      final << recipe_hash
+      unless !(recipe.ingredients & user.restricted_ings).empty?
+        recipe_hash = {}
+        recipe_hash[:recipe] = recipe
+        # count number of recipe ingredients missing from user's kitchen
+        diff = recipe.ingredients - user.ingredients
+        recipe_hash[:missing] = diff.length
+        final << recipe_hash
+      end
+    end
+    # return sorted array
+    final = final.sort_by { |recipe| recipe[:missing] }
+    final
+  end
+
+  def self.user_include_restrictions(user)
+    # array of hashes to return
+    final = []
+    # loop over each recipe
+    self.all.each do |recipe|
+        recipe_hash = {}
+        recipe_hash[:recipe] = recipe
+        # count number of recipe ingredients missing from user's kitchen
+        diff = recipe.ingredients - user.ingredients
+        recipe_hash[:missing] = diff.length
+        final << recipe_hash
     end
     # return sorted array
     final = final.sort_by { |recipe| recipe[:missing] }
